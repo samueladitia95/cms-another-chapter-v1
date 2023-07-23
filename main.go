299cd2eb867abde88d32ea9dd976d5ef5727e5f8
Samuel Aditia <samuel.aditia@ruparupa.com>
2023-07-23 21:25:21 +0700

    update dependencies

--- main.go
+++ main.go
@@ -3,12 +3,25 @@ package main
 
 import (
 	"log"
+	"os"
+	"strings"
 
 	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/plugins/migratecmd"
 )
 
 func main() {
 	app := pocketbase.New()
+
+	// loosely check if it was executed using "go run"
+	isGoRun := strings.HasPrefix(os.Args[0], os.TempDir())
+
+	migratecmd.MustRegister(app, app.RootCmd, &migratecmd.Options{
+		// enable auto creation of migration files when making collection changes
+		// (the isGoRun check is to enable it only during development)
+
+		Automigrate: isGoRun,
+	})
 
 	if err := app.Start(); err != nil {
 		log.Fatal(err)
